956913d33f4061c2625f81ee00027854fbb1ba80
DavidPalmgren <swemultigamers@gmail.com>
2024-10-30 16:28:32 +0100

    removing array

--- Infrastructure/Documents/docModule.mjs
+++ Infrastructure/Documents/docModule.mjs
@@ -4,7 +4,7 @@ const documentSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String},
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    code: [{type: Boolean, required: true}]
+    code: {type: Boolean, required: true}
 });
 const MongoDocument = mongoose.model('Document', documentSchema);
 
